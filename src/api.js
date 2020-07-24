27f42e753704e9bfcd546d49f187bf9b01d6237c
Vincent Falcone <vincent.falcone@outlook.com>
2020-07-24 15:01:00 -0400

    testing stuff

--- src/api.js
+++ src/api.js
@@ -7,17 +7,3 @@ const imageLookup = new ImageLookup();
 
 const API_PREFIX = '/api/v1';
 
-module.exports = app => {
-  app.get(`${API_PREFIX}/image-lookup`, imageLookup.get);
-
-  app.post(`${API_PREFIX}/login`, (...args) => login.login(...args));
-  app.get(`${API_PREFIX}/logout`, (req, res) =>
-    req.session.destroy(() => res.redirect('/login'))
-  );
-
-  app.post(`${API_PREFIX}/order`, (...args) => order.addToOrder(...args));
-  app.post(`${API_PREFIX}/order/checkout`, (...args) =>
-    order.checkout(...args)
-  );
-  app.delete(`${API_PREFIX}/order`, (...args) => order.removeOrder(...args));
-};
